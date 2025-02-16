ae18cbe7c1a8bbc959184ee60e7c5cdf52e8a4b0
lidiya-fiker <lidiyafikerr@gmail.com>
2025-02-16 20:23:27 +0300

    sign out

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,18 +1,20 @@
-import { Injectable,NotFoundException } from '@nestjs/common';
-import {  Repository } from 'typeorm';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
 @Injectable()
 export class UsersService {
-  constructor(@InjectRepository(User) private repo: Repository<User>) { }
-  
+  constructor(@InjectRepository(User) private repo: Repository<User>) {}
 
   async create(email: string, password: string) {
     const user = this.repo.create({ email, password });
     return await this.repo.save(user);
   }
 
-  findOne(id: number) { 
+  findOne(id: number) {
+    if (!id) {
+      return null;
+    }
     return this.repo.findOne({ where: { id } });
   }
 
@@ -37,6 +39,4 @@ export class UsersService {
     }
     return this.repo.remove(user);
   }
-
-  
 }
