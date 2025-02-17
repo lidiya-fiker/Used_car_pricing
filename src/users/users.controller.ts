e1ca28af7ff14facdd8f98226dc46bf5edaf35fa
lidiya-fiker <lidiyafikerr@gmail.com>
2025-02-17 10:58:10 +0300

    current user interceptor and decorator

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -9,6 +9,7 @@ import {
   Query,
   NotFoundException,
   Session,
+
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
@@ -16,18 +17,27 @@ import { UsersService } from './users.service';
 import { Serialize } from 'src/interceptors/serialize.interceptors';
 import { UserDto } from './dtos/User.dto';
 import { AuthService } from './auth.service';
+import { CurrentUser } from './decorators/current-user.decorators';
+
+import { User } from './user.entity';
 
 @Controller('auth')
 @Serialize(UserDto)
+
 export class UsersController {
   constructor(
     private userService: UsersService,
     private authService: AuthService,
   ) {}
 
-  @Get('/Whoami')
-  WhoAmI(@Session() session: any) {
-    return this.userService.findOne(session.userId);
+  // @Get('/Whoami')
+  // WhoAmI(@Session() session: any) { 
+  //   return this.userService.findOne(session.userId);
+  // }
+
+  @Get('/whoami')
+  whoAmI(@CurrentUser() user: User) {
+    return user;
   }
 
   @Post('/signout')
