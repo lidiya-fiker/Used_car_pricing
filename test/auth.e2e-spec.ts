757c80a9cbb6ff13370e16bb5482d7e428723608
lidiya-fiker <lidiyafikerr@gmail.com>
2025-02-21 16:48:26 +0300

    e2e testing update

--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -16,7 +16,7 @@ describe('Authentication System (e2e)', () => {
   });
 
   it('handles a signup request ', () => {
-    const email = 'asdlkjq123 @akl.com';
+    const email = 'asdlkjq12@akl.com';
 
     return request(app.getHttpServer())
       .post('/auth/signup')
@@ -27,5 +27,22 @@ describe('Authentication System (e2e)', () => {
         expect(id).toBeDefined();
         expect(email).toEqual(email);
       });
+  });
+  it('signup as a new user then get the currently logged in user', async () => {
+    const email = 'asdf@asdf.com';
+
+    const res = await request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({ email, password: 'asdf' })
+      .expect(201);
+
+    const cookie = res.get('Set-Cookie');
+
+    const { body } = await request(app.getHttpServer())
+      .get('/auth/whoami')
+      .set('Cookie', cookie)
+      .expect(200);
+
+    expect(body.email).toEqual(email);
   });
 });
