79cf71aa930cc5e8d90371a8ce349e1df5b918bd
lidiya-fiker <lidiyafikerr@gmail.com>
2025-02-25 07:13:01 +0300

    post report

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,4 +1,5 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { Report } from 'src/reports/report.entity';
+import { Entity, Column, PrimaryGeneratedColumn, OneToMany } from 'typeorm';
 @Entity()
 export class User {
   @PrimaryGeneratedColumn()
@@ -9,4 +10,7 @@ export class User {
 
   @Column()
   password: string;
+
+  @OneToMany(()=>Report,(report)=>report.user)
+  reports: Report[];
 }
